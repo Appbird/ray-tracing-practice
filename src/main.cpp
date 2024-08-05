22a98bbfc2ea2881e77db10e08545986bb276709
appbird <project.appbird@gmail.com>
2024-08-05 19:45:50 +0900

    simplify code in for-loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,6 @@
+#include "vec3.hpp"
+#include "color.hpp"
+
 #include <iostream>
 
 int main() {
@@ -8,18 +11,11 @@ int main() {
     std::cout << image_width << " " << image_height << std::endl;
     std::cout << 255 << std::endl;
 
-    for (int32_t i = 0; i < image_width; i++) {
-        std::clog << "\rScanlines remaining: " << (image_height - i) << " " << std::flush;
-        for (int32_t j = 0; j < image_height; j++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_width - 1);
-            auto b = 0.0;
-
-            int32_t ir = int32_t(255.999 * r);
-            int32_t ig = int32_t(255.999 * g);
-            int32_t ib = int32_t(255.999 * b);
-
-            std::cout << ir << " " << ig << " " << ib << std::endl;
+    for (int32_t j = 0; j < image_width; j++) {
+        std::clog << "\rScanlines remaining: " << (image_height - j) << " " << std::flush;
+        for (int32_t i = 0; i < image_height; i++) {
+            color pixel_color = color{double(i) / (image_width - 1), double(j) / (image_width - 1), 0};
+            write_color(std::cout, pixel_color);
         }
     }
     std::clog << "\rDone.                       \n" << std::flush;
