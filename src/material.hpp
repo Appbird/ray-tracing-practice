66be06334a68d275f81ad97afbb9cca492e9a39f
appbird <project.appbird@gmail.com>
2024-08-07 18:48:45 +0900

    implements Snell's Law and Total Reflection

--- src/material.hpp
+++ src/material.hpp
@@ -3,6 +3,9 @@
 
 #include "rtweekend.hpp"
 
+using std::min;
+using std::max;
+
 class material {
     public:
     virtual ~material() = default;
@@ -18,16 +21,16 @@ class material {
      * @return false 
      */
     virtual bool scatter(
-        const ray& r_in,
-        const hit_record& rec,
-        color& attenuation,
-        ray& scattered
+        [[maybe_unused]] const ray& r_in,
+        [[maybe_unused]] const hit_record& rec,
+        [[maybe_unused]] color& attenuation,
+        [[maybe_unused]] ray& scattered
     ) const {
         return false;
     }
 
 };
-
+// ランバート反射に従うマテリアル
 class lambertian : public material {
     private:
         color albedo;
@@ -35,7 +38,7 @@ class lambertian : public material {
     lambertian(const color& albedo) : albedo(albedo) {}
 
     bool scatter(
-        const ray& r_in,
+        [[maybe_unused]] const ray& r_in,
         const hit_record& rec,
         color& attenuation,
         ray& scattered
@@ -52,6 +55,7 @@ class lambertian : public material {
 
 };
 
+// 金属マテリアル
 class metal : public material {
     private:
         color albedo;
@@ -67,9 +71,59 @@ class metal : public material {
         ) const override {
             vec3 reflected = reflect(r_in.direction(), rec.normal);
             reflected = unit_vector(reflected) + (fuzz * random_in_unit_sphere());
-            scattered = ray(rec.p, reflected);
+            scattered = ray{rec.p, reflected};
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
+        }
+};
+
+// 絶縁体
+class dielectric : public material { 
+    private:
+        /**
+         * Refractive index in vacuum or air,
+         * or the ratio of the material's refractive index over
+         * the refractive index of the enclosing media
+         */
+        double refraction_index;
+    public:
+        dielectric(double refraction_index) : refraction_index(refraction_index) {}
+        bool scatter(
+            const ray& r_in,
+            const hit_record& rec,
+            color& attenuation,
+            ray& scattered
+        ) const override {
+            attenuation = color{1.0, 1.0, 1.0};
+            // 表面から入ってきたか内側から入ってきたかで屈折率が変わる
+            double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+            const vec3 unit_direction = unit_vector(r_in.direction());
+            const double cos_theta = min(1.0, dot(-unit_direction, rec.normal));
+            const double sin_theta = std::sqrt(1 - cos_theta*cos_theta);
+            const bool can_refract = (ri*sin_theta <= 1.0);
+            
+            const vec3 next_direction =
+                (can_refract)
+                ? reflact(unit_direction, rec.normal, ri)
+                : reflect(unit_direction, rec.normal);
+            scattered = ray{rec.p, next_direction};
+            return true;
+        }
+        /**
+         * @brief Schlickの近似式を用いて反射係数（反射率）を求める
+         * 
+         * @param cosine 入射角のコサイン値
+         * @param refraction_index 屈折率
+         * @return double 反射率
+         */
+        static double reflectance(
+            const double cosine,
+            const double refraction_index
+        ) {
+            double r0 = (1 - refraction_index) / (1 + refraction_index);
+            r0 = r0 * r0;
+            return r0 + (1 - r0) * std::pow(1 - cosine, 5);
         }
 };
 
